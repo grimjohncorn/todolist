926379a2e6ececad9c3ae09fdf45aa9475a807c7
chris grimley <chrismessage55@outlook.com>
2019-09-10 20:07:58 +0100

    Updates

---
+++ precache-manifest.8e6798b8858c23c03921635687b4eaf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8542d250d7be90aba3780a49b2d87d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c9722eb45aaed0eafe64",
+    "url": "/todolist/static/css/main.b69a3f28.chunk.css"
+  },
+  {
+    "revision": "be2996f816799594fa61",
+    "url": "/todolist/static/js/2.131c0489.chunk.js"
+  },
+  {
+    "revision": "c9722eb45aaed0eafe64",
+    "url": "/todolist/static/js/main.ecc125b6.chunk.js"
+  },
+  {
+    "revision": "494ea65bfa3a7b566acf",
+    "url": "/todolist/static/js/runtime~main.b0077857.js"
+  },
+  {
+    "revision": "a13b05ac5372c20714aada3caa9c8a4d",
+    "url": "/todolist/static/media/background-todo.a13b05ac.jpg"
+  }
+]);
